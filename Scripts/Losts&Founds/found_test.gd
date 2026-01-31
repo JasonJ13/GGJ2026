2351980f25e30d9c172d266ab1d0a7208553be85
Eline-eline-eline <elineagathe@gmail.com>
2026-01-31 13:12:29 +0100

    Eline3

--- Scripts/Losts&Founds/found_test.gd
+++ Scripts/Losts&Founds/found_test.gd
@@ -1,14 +1,16 @@
 extends Control
-signal grab_signal
+signal place_signal
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
 func _on_button_pressed() -> void:
-	print("lost grab")
-	grab_signal.emit()
+	print("found clic")
+	place_signal.emit()
--- Scripts/Rooms/TestRoom.gd
+++ Scripts/Rooms/TestRoom.gd
@@ -6,9 +6,13 @@ Salle précédente : Salle de test
 
 extends Room
 @onready var lost_test_child: Control = $LostTest
+@onready var found_test_child: Control = $FoundTest
+
+var lost_positions
 
 func _ready() -> void:
 	lost_test_child.grab_signal.connect(_on_lost_test_grab_signal)
+	found_test_child.place_signal.connect(_on_found_test_grab_signal)
 
 func _init() -> void:
 	name_room = "Test Room "
@@ -21,3 +25,8 @@ func set_num(num : int) :
 func _on_lost_test_grab_signal() -> void:
 	print("chek room lost grab")
 	self.remove_child(lost_test_child)
+	
+func _on_found_test_grab_signal() -> void:
+	print("chek room found place")
+	self.remove_child(found_test_child)
+	self.add_child(lost_test_child)
