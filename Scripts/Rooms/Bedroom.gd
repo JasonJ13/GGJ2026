317af6694048ade5fe36dcb571bd26e19aae954c
Eline-eline-eline <elineagathe@gmail.com>
2026-01-31 20:18:04 +0100

    Eline8

--- Scripts/Rooms/Bedroom.gd
+++ Scripts/Rooms/Bedroom.gd
@@ -3,7 +3,7 @@ extends Room
 
 # Called when the node enters the scene tree for the first time.
 func _init() -> void:
-	name_room = "bedroom"
+	name_room = "livingroom"
 
 func get_lost_test() -> Lost :
 	return $LostTest
