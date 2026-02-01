3bb6501491dd55e67a2183cd3e965a3a038e6bf8
JasonJ13 <jason.jallabert@orange.fr>
2026-02-01 17:30:07 +0100

    room found lost

--- Scripts/Rooms/Attic.gd
+++ Scripts/Rooms/Attic.gd
@@ -1,5 +1,5 @@
 extends Room
 
-var pos1: Vector2 = Vector2(277.0,515.0)
-var pos2:Vector2 = Vector2(796.0,240.0)
-var poos3:Vector2 = Vector2(897.0,514.0)
+
+func _ready() -> void:
+	set_random_pos_lost(get_random_lost())
--- Scripts/Rooms/Room.gd
+++ Scripts/Rooms/Room.gd
@@ -9,6 +9,7 @@ var right_room : Room
 signal grab_signal(lost:Lost)
 signal place_signal(found:Found)
 
+
 func set_left_room(lRoom : Room) -> void:
 	left_room = lRoom
 
@@ -35,11 +36,26 @@ func lost_clicke(lost : Lost) -> void :
 
 
 @export var losts : Array[Lost]
-var nmb_lost : int = len(losts)
+@export var nmb_losts : int
 
 func get_random_lost() -> Lost :
-	var chance : int = randi() % nmb_lost
+	print(losts)
+	print(nmb_losts)
+	var chance : int = randi() % nmb_losts
 	
-	return losts[chance]
+	var lost = losts[chance]
 	
+	lost.found_associate.texture_associate.hide()
 	
+	return lost
+
+@export var posLosts : Array[Vector2]
+@export var nmb_posLosts : int
+
+func set_random_pos_lost(lost : Lost) -> void :
+	add_child(lost)
+	
+	lost.show()
+	lost.isLost = true
+	
+	lost.position = posLosts[randi() % nmb_posLosts]
